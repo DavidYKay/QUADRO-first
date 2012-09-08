5b4ac4c88b71a7cb9943a1bb6e8b2c7a1cc03b6c
David Young-Chan Kay <davidykay@gmail.com>
2012-09-08 15:40:23 -0700

    corrected ardrone to use 2.0 SDK movement command

--- gamepad.c
+++ gamepad.c
@@ -335,7 +335,8 @@ C_RESULT update_gamepad(void) {
 	if (refresh_values) {
 
 		g_autopilot = FALSE;
-		ardrone_at_set_progress_cmd(enable, phi, theta, gaz, yaw);
+		//ardrone_at_set_progress_cmd(enable, phi, theta, gaz, yaw);
+		ardrone_tool_set_progressive_cmd(enable, phi, theta, gaz, yaw, 0.0, 0.0);
 	}
 
 	return C_OK;
